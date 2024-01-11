1d4ba6df5b242ef0ccfb9ab2b39efd5a5550c058
Parva <73553969+DOPO2@users.noreply.github.com>
2024-01-11 23:02:15 +0530

    Add files via upload

---
+++ Doctor.java
@@ -0,0 +1,56 @@
+package HospitalManagementSystem;
+
+import javax.xml.transform.Result;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+import java.sql.ResultSet;
+
+public class Doctor {
+    private Connection connection;
+
+
+    public Doctor(Connection connection){
+        this.connection = connection;
+    }
+
+    public void viewDoctors(){
+        String query = "Select * from doctors";
+        try{
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            System.out.println("Doctors: ");
+            System.out.println("+------------+-------------------+-------------------------+");
+            System.out.println("| Doctor ID  |    Doctor Name    |     Specialization      |");
+            System.out.println("+------------+-------------------+-------------------------+");
+            while(resultSet.next()){
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String specialization = resultSet.getString("specialization");
+                System.out.printf("| %-10s | %-17s | %-23s |\n", id, name, specialization);
+                System.out.println("+------------+-------------------+-------------------------+");
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public boolean getDoctorById(int id){
+        String query = "Select * from doctors where id = ?";
+        try{
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()){
+                return true;
+            }else{
+                return false;
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
